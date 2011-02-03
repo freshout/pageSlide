bc450452206ae3d05a605caa8247db3f304bc60e
felix <cf@freshout.us>
2011-02-03 09:41:46 -0600

    removing unnecessary settings

--- pageSlide.js
+++ pageSlide.js
@@ -1,10 +1,3 @@
-/*
- * PageSlide jQuery plugin.
- * 
- * # Modification of the Scott Robbin's pageSlide plugin
- * # Check it out at http://github.com/freshout/pageSlide
- *
-*/
 $.fn.pageSlide = function(opts) {
   var settings = $.extend({
     height: '85%',
@@ -40,13 +33,12 @@ $.fn.pageSlide = function(opts) {
       }
     },     
     duration: 'normal',
-    direction: 'top',
-    opacity: 0.6,
-    modal: false
+    direction: 'top'
   }, opts);
 
   var page = $(document),
       body = $('body'),
+      htmlDom = $('html'),
       collection = $(this),
       size = collection.size(),                          
       current = false;
@@ -75,31 +67,23 @@ $.fn.pageSlide = function(opts) {
 
     $('<div />').attr('id', 'pageslide-slide-wrap').append(psSlideContent).appendTo(body);
 
-    if($('#pageslide-blanket').size() === 0 && settings.modal === true) {
-      var psSlideBlanket = $('<div class="pageslide-close"/>').attr('id', 'pageslide-blanket');
-      body.append(psSlideBlanket);
-      psSlideBlanket.click(function() {
-        return false;
-      });
-    }
-
     $(window).resize(function() {
     $('#pageslide-body-wrap').width(body.width());
     });
 
     $(anchor).attr('rel', 'pageslide'); 
     
-    bindButtons();
+    _bindButtons();
   }
   
-  function bindButtons() {
+  function _bindButtons() {
     var psIframe = $('#pageslide-iframe');
     $('.view-original').unbind('click').click(function(){
       window.location = psIframe.attr('src')
     });
 
     $('.pageslide-close').unbind('click').click(function(ev) {
-      _closeSlide(ev);
+      collection.trigger('closePageSlide');
       return false;
     });
 
@@ -107,12 +91,12 @@ $.fn.pageSlide = function(opts) {
       current = $(this).hasClass('next') ? ((current == size-1) ? 0 : current+1) : ((current == 0) ? size-1 : current-1);
       var url = collection.eq(current).attr('href');
       psIframe.attr('src', url);
-      settings.buttons.bookmarkers.display && changeFacebookURL(url);
+      settings.buttons.bookmarkers.display && _changeFacebookURL(url);
       return false;
     });
   };
   
-  function changeFacebookURL(url){
+  function _changeFacebookURL(url){
     $('.share-facebook').unbind('click').click(function(){
       window.open("http://www.facebook.com/sharer.php?u="+encodeURIComponent(url), 'sharer', 'toolbar=0,status=0,width=626,height=436');
       return false;
@@ -127,7 +111,6 @@ $.fn.pageSlide = function(opts) {
     if(psWrap.height() != 0) { 
       return false;
     }
-    _showBlanket();
     var direction = {};
     var new_height = settings.height.charAt(settings.height.length - 1) == '%' ? Math.ceil($(window).height() * parseFloat(settings.height) / 100) + 'px' : settings.height;
     if(settings.direction === 'bottom') {
@@ -143,63 +126,63 @@ $.fn.pageSlide = function(opts) {
       psWrap.css('bottom', 'auto');
     }
     body.attr('scrollTop', 0);
-    _overflowFixAdd();                                                   
+    body.add(htmlDom).css({overflowY: 'hidden'});
     
-    $('#pageslide-body-wrap, #pageslide-slide-wrap').width(body.width());
     psWrap.animate({height: new_height}, settings.duration);
     psBodyWrap.animate(direction, settings.duration, function() {        
-      body.css('position', 'relative').css('height', $(window).height());
-      psContent.css('height', psWrap.height() - 42).show();
+      psContent.css('height', psWrap.height() - 40).show();
       settings.loading && psIframe.css('height', 0);
-      $(elm).attr('href') !== psIframe.attr('src') && psIframe.attr('src', elm.href) && settings.buttons.bookmarkers.display && changeFacebookURL(elm.href);
+      $(elm).attr('href') !== psIframe.attr('src') && psIframe.attr('src', elm.href) && settings.buttons.bookmarkers.display && _changeFacebookURL(elm.href);
 
       $(window).resize(function() {
         new_height = settings.height.charAt(settings.height.length - 1) == '%' ? Math.ceil($(window).height() * parseFloat(settings.height) / 100) + 'px' : settings.height;
         psWrap.css('height', new_height);
-        psContent.css('height', psWrap.height() - 42);
+        psContent.css('height', psWrap.height() - 40);
         psBodyWrap.css('top', new_height);
       });
 
       settings.buttons.copy_url.display && loadZeroClipboard('glueButton', function(){
-        $('.psMessage').html(settings.buttons.copy_url.success);
+        var psMessage = $('.psMessage');
+        psMessage.html(settings.buttons.copy_url.success);
         setTimeout(function(){
-          $('.psMessage').html('&nbsp;');
+          psMessage.html('&nbsp;');
         }, 5000);
       }) && setZeroClipboardText(psIframe.attr('src'));
     });
   };
-    
-  function _closeSlide(event) {
-    (event.button != 2 && $('#pageslide-slide-wrap').height() != 0) && $.fn.pageSlideClose(settings);
-  }
-
-  function _showBlanket() {
-    settings.modal === true && $('#pageslide-blanket').toggle().animate({opacity: settings.opacity}, 'fast', 'linear');
-  }
-
-  function _overflowFixAdd() {
-    $('html, body').css({overflowY: 'hidden'});
-  }
+  
+  collection.bind('closePageSlide', function(event){
+      if(event.button != 2 && $('#pageslide-slide-wrap').height() != 0){
+        body.css('height', 'auto');
+        $('#pageslide-slide-wrap').animate({height: 0}, settings.duration, function() {
+          $('#pageslide-content').css('height', '0px').hide();
+          $('#pageslide-body-wrap, #pageslide-slide-wrap').css({
+            top: '0px',
+            bottom: '0px',
+            width: ''
+          });
+          $(window).unbind('resize');
+          $('#pageslide-iframe').attr('src', 'about:blank');
+          body.add(htmlDom).css({overflowY: ''});
+        });
+      }
+  });  
+  
 
   _initialize(this);
 
+  page.click(function(ev) {
+    if(ev.target.tagName != "A") {
+      collection.trigger('closePageSlide');
+      return false;
+    }
+  });
+  
   return this.each(function() {
     var link = $(this);
     if(!link.hasClass('pageslide-binded')) {
       link.bind('click', function(e) {
         _openSlide(this);
-        $('#pageslide-slide-wrap').unbind('click').click(function(e) {
-          if(e.target.tagName != "A") { return false; }
-        });
-        if(settings.modal != true && !page.hasClass('pageslide-binded')) {
-          page.click(function(ev) {
-            if(e.target.tagName != "A") {
-              _closeSlide(ev);
-              return false;
-            }
-            $(this).addClass('pageslide-binded');
-          });
-        }
         link.addClass('pageslide-binded');
         return false;
       });
@@ -207,45 +190,8 @@ $.fn.pageSlide = function(opts) {
   });
 };
 
-$.fn.pageSlideClose = function(opts) {
-    var settings = $.extend({
-        height: '300px',
-        duration: 'normal',
-        direction: 'top',
-        modal: false,
-        _identifier: $(this)
-    }, opts);
-    
-    function _hideBlanket() {
-      settings.modal === true && $('#pageslide-blanket').is(':visible') && $('#pageslide-blanket').animate({opacity: 0}, 'fast', 'linear', function() {
-        $(this).hide();
-      });
-    }
-    
-    function _overflowFixRemove() {
-        $('html, body').css({overflowY: ''});
-    }              
-    
-    _hideBlanket();
-    
-    $('body').css('height', 'auto');
-    $('#pageslide-slide-wrap').animate({
-        height: 0
-    }, settings.duration, function() {
-        $('#pageslide-content').css('height', '0px').hide();
-        $('#pageslide-body-wrap, #pageslide-slide-wrap').css({
-            top: '0px',
-            bottom: '0px',
-            width: ''
-        });
-        $(window).unbind('resize');
-        $('#pageslide-iframe').attr('src', 'about:blank');
-        _overflowFixRemove();
-    });
-};
-
 $(function() {
-    $(document).keyup(function(event) {
-        if ($("#pageslide-blanket").is(":visible") && event.keyCode == 27) {$(this).pageSlideClose({modal: true});}
-    });
+  $(document).keyup(function(event) {                                     
+    $("#pageslide-blanket").is(":visible") && event.keyCode == 27 && $('a').trigger('closePageSlide');
+  });
 });
